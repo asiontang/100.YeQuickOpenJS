64c082508d3c0be77a4844368650ede147ab011c
AsionTang <none@none>
2019-12-19 17:38:30 +0800

    +增加了:下方常驻的已经选择的目录列表.方便快速切换常用目录.

--- app/src/main/java/cn/asiontang/app/quick_open_file/MainActivity.java
+++ app/src/main/java/cn/asiontang/app/quick_open_file/MainActivity.java
@@ -2,18 +2,25 @@ package cn.asiontang.app.quick_open_file;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import androidx.documentfile.provider.DocumentFile;
@@ -27,17 +34,46 @@ import eu.chainfire.libsuperuser.Shell;
 @SuppressLint({"StaticFieldLeak", "DefaultLocale"})
 public class MainActivity extends Activity
 {
-    private static final String KEY_STR_SELECTED_FOLDER_URI = "UserSelectedFolderUri";
+    private static final String KEY_INT_SELECTED_FOLDER_INDEX = "UserSelectedFolderUriIndex";
+    private static final String KEY_STR_SELECTED_FOLDER_URI_LIST = "UserSelectedFolderUriList";
     private static final String TAG = "-------";
     private final List<YeFile> mFileList = new ArrayList<>();
+    private final List<Uri> mUserSelectedFolderUriList = new ArrayList<>();
     private BaseAdapterEx3<YeFile> mAdapter;
-    private Uri mUserSelectedFolderUri;
+    private int mUserSelectedFolderUriIndex = 0;
+    private SharedPreferences mDefaultSharedPreferences;
+    private RadioGroup mLayoutFolderItems;
+
+    private void excludeTheFile(final YeFile item)
+    {
+        getDefaultSharedPreferences().edit().putBoolean(item.mDocumentFile.getUri().toString(), true).apply();
+        if (mAdapter.getOriginaItems() == null)
+            return;
+
+        mAdapter.getOriginaItems().remove(item);
+        mAdapter.refresh();
+
+        Toast.makeText(this, item.Name + " 排除成功", Toast.LENGTH_SHORT).show();
+    }
+
+    private SharedPreferences getDefaultSharedPreferences()
+    {
+        if (mDefaultSharedPreferences != null)
+            return mDefaultSharedPreferences;
+        return mDefaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+    }
 
     private void initData()
     {
-        final String uriStr = PreferenceManager.getDefaultSharedPreferences(this).getString(KEY_STR_SELECTED_FOLDER_URI, null);
-        if (!TextUtils.isEmpty(uriStr))
-            mUserSelectedFolderUri = Uri.parse(uriStr);
+        SharedPreferences defaultSharedPreferences = getDefaultSharedPreferences();
+
+        mUserSelectedFolderUriIndex = defaultSharedPreferences.getInt(KEY_INT_SELECTED_FOLDER_INDEX, 0);
+
+        final String[] uriStr = defaultSharedPreferences.getString(KEY_STR_SELECTED_FOLDER_URI_LIST, "").split("\f");
+        if (uriStr.length > 0)
+            for (String s : uriStr)
+                if (!TextUtils.isEmpty(s))
+                    mUserSelectedFolderUriList.add(Uri.parse(s));
     }
 
     private void initView()
@@ -53,13 +89,12 @@ public class MainActivity extends Activity
                 viewHolder.getTextView(android.R.id.title).setText(item.Name);
             }
         });
-        Toast.makeText(this, "长按任意项重新选择目录", Toast.LENGTH_SHORT).show();
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
         {
             @Override
             public boolean onItemLongClick(final AdapterView<?> parent, final View view, final int position, final long id)
             {
-                startSelectFolder();
+                excludeTheFile(mAdapter.getItem(position));
                 return false;
             }
         });
@@ -106,12 +141,39 @@ public class MainActivity extends Activity
                 //startActivity(intent);
             }
         });
+
+        mLayoutFolderItems = findViewById(R.id.layoutFolderItems);
+        mLayoutFolderItems.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(final RadioGroup group, final int checkedId)
+            {
+                if (checkedId == -1)
+                    return;
+                RadioButton view = group.findViewById(checkedId);
+                if (view == null || !view.isChecked())
+                    return;
+                mUserSelectedFolderUriIndex = mLayoutFolderItems.indexOfChild(view) - 2/*排除前面默认的+号按钮和|分隔符*/;
+                getDefaultSharedPreferences().edit().putInt(KEY_INT_SELECTED_FOLDER_INDEX, mUserSelectedFolderUriIndex).apply();
+
+                loadFiles((Uri) view.getTag());
+            }
+        });
+
+        findViewById(R.id.btnAdd).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(final View v)
+            {
+                startSelectFolder();
+            }
+        });
     }
 
     /**
      * <li><a href="https://stackoverflow.com/questions/26744842/how-to-use-the-new-sd-card-access-api-presented-for-android-5-0-lollipop">How to use the new SD card access API presented for Android 5.0 (Lollipop)? - Stack Overflow</a></li>
      */
-    private void loadFiles()
+    private void loadFiles(Uri mUserSelectedFolderUri)
     {
         try
         {
@@ -121,9 +183,13 @@ public class MainActivity extends Activity
             DocumentFile pickedDir = DocumentFile.fromTreeUri(this, mUserSelectedFolderUri);
 
             // List all existing files inside picked directory
+            mFileList.clear();
             for (DocumentFile file : pickedDir.listFiles())
             {
                 if (!file.isFile())
+                    continue;
+                //过滤掉手动排除过的文件
+                if (getDefaultSharedPreferences().getBoolean(file.getUri().toString(), false))
                     continue;
                 mFileList.add(new YeFile(file));
             }
@@ -142,9 +208,21 @@ public class MainActivity extends Activity
         if (resultCode != RESULT_OK || data == null || data.getData() == null)
             return;
 
-        mUserSelectedFolderUri = data.getData();
-        PreferenceManager.getDefaultSharedPreferences(this).edit().putString(KEY_STR_SELECTED_FOLDER_URI, data.getData().toString()).apply();
-        loadFiles();
+        if (!mUserSelectedFolderUriList.contains(data.getData()))
+            mUserSelectedFolderUriList.add(data.getData());
+
+        //添加选择的目录到配置文件里
+        SharedPreferences defaultSharedPreferences = getDefaultSharedPreferences();
+        String[] uriSet = defaultSharedPreferences.getString(KEY_STR_SELECTED_FOLDER_URI_LIST, "").split("\f");
+        List<String> list = new ArrayList<>(Arrays.asList(uriSet));
+        list.add(data.getData().toString());
+        defaultSharedPreferences.edit().putString(KEY_STR_SELECTED_FOLDER_URI_LIST, TextUtils.join("\f", list.toArray(new String[0]))).apply();
+
+        //加载当前目录的最新数据
+        loadFiles(data.getData());
+
+        //刷新目录列表
+        refreshUserSelectedFolderItemList();
     }
 
     @Override
@@ -156,10 +234,66 @@ public class MainActivity extends Activity
 
         initView();
 
-        if (mUserSelectedFolderUri == null)
+        //刷新目录列表
+        refreshUserSelectedFolderItemList();
+
+        if (mUserSelectedFolderUriList.isEmpty())
             startSelectFolder();
         else
-            loadFiles();
+            loadFiles(mUserSelectedFolderUriList.get(Math.min(mUserSelectedFolderUriIndex, mUserSelectedFolderUriList.size() - 1)));
+    }
+
+    private void refreshUserSelectedFolderItemList()
+    {
+        //先移除旧的.
+        if (mLayoutFolderItems.getChildCount() > 2)
+            mLayoutFolderItems.removeViews(2, mLayoutFolderItems.getChildCount() - 2);
+
+        //再添加新的.
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        for (int i = 0; i < mUserSelectedFolderUriList.size(); i++)
+        {
+            final Uri uri = mUserSelectedFolderUriList.get(i);
+            final RadioButton child = (RadioButton) layoutInflater.inflate(R.layout.activity_main_folder_item, mLayoutFolderItems, false);
+            child.setId(i);
+            child.setTag(uri);
+            child.setText(uri.getLastPathSegment().substring(uri.getLastPathSegment().indexOf("/") + 1));
+            child.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(final View v)
+                {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setMessage("是否删除" + child.getText())
+                            .setNegativeButton(android.R.string.no, null)
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener()
+                            {
+                                @Override
+                                public void onClick(final DialogInterface dialog, final int which)
+                                {
+                                    int index = mLayoutFolderItems.indexOfChild(child);
+
+                                    mUserSelectedFolderUriList.remove(uri);
+                                    mLayoutFolderItems.removeView(child);
+
+                                    loadFiles(mUserSelectedFolderUriList.get(Math.min(index, mUserSelectedFolderUriList.size() - 1)));
+
+                                    String[] uriSet = getDefaultSharedPreferences().getString(KEY_STR_SELECTED_FOLDER_URI_LIST, "").split("\f");
+                                    {
+                                        List<String> list = new ArrayList<>(Arrays.asList(uriSet));
+                                        list.remove(uri.toString());
+                                        getDefaultSharedPreferences().edit().putString(KEY_STR_SELECTED_FOLDER_URI_LIST, TextUtils.join("\f", list.toArray(new String[0]))).apply();
+                                    }
+
+                                    Toast.makeText(MainActivity.this, child.getText() + " 删除成功", Toast.LENGTH_SHORT).show();
+                                }
+                            })
+                            .show();
+                    return false;
+                }
+            });
+            mLayoutFolderItems.addView(child);
+        }
     }
 
     private void startSelectFolder()
